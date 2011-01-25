2c0e348b993a754df006073c5dad71e4237f58d7
alexandru.topliceanu <alex.topliceanu@gmail.com>
2011-01-25 23:19:08 +0200

    initial commit

---
+++ sum.js
@@ -0,0 +1,10 @@
+(function summarization(win,doc,undef){
+	
+	var newWord = function( params ) {
+		if( params.word === undef ) throw new Error('params.word must be set') ;
+
+		var lsf = params.lsf || 0 ;
+		var lss = params.lss || 0 ;
+	}
+
+})(window,document) ;
