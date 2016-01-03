82cfa105532cc0b24bf9a461e22f2395c346f553
alexandru topliceanu <alexandru.topliceanu@gmail.com>
2016-01-03 12:59:05 +0000

    update to node latests lts version 4.2.4

--- vagrant_bootstrap.sh
+++ vagrant_bootstrap.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/env bash
 
 # Init.
-apt-get update
+sudo apt-get update
 
-# Install node.
-NODE_VERSION=0.10.35
-apt-get -y install g++ gcc make
-wget http://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION.tar.gz -O /tmp/nodejs.tar.gz
-tar -xzvf /tmp/nodejs.tar.gz -C /home/vagrant
-chown -R vagrant:vagrant /home/vagrant/node-v$NODE_VERSION
-su - vagrant -c "/home/vagrant/node-v$NODE_VERSION/configure"
-su - vagrant -c "cd /home/vagrant/node-v$NODE_VERSION; make"
-su - vagrant -c "cd /home/vagrant/node-v$NODE_VERSION; sudo make install"
+# Install nvm
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.30.1/install.sh | bash
+echo ". ~/.nvm/nvm.sh" >> ~/.bash_profile
+source ~/.bash_profile
 
-# Install node dependencies.
-su - vagrant -c "cd /vagrant/; npm install"
+# Install node v4.2.4
+nvm install 4
